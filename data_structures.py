da8aca598ec65be7c8949f5876f8dc0e0ce058ec
Reginald Edwards <reginald.edwards@gmail.com>
2020-07-27 21:40:28 -0500

    Chapter 3 work

---
+++ data_structures.py
@@ -0,0 +1,35 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        return self.items.insert(0, item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[0]
+
+    def size(self):
+        return len(self.items)
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        return self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
